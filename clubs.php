5490c1a24e2a89d57499b45aa6323f1907964448
lizardcat <alex.raza.ke@gmail.com>
2025-07-24 12:51:21 +0300

    Adding club page

---
+++ clubs.php
@@ -0,0 +1,3 @@
+<?php include 'includes/db.php'; ?>
+<?php include 'includes/head.php'; ?>
+<?php include 'includes/header.php'; ?>
--- includes/header.php
+++ includes/header.php
@@ -3,11 +3,12 @@
 <header>
     <h1>USIU Campus Events</h1>
     <nav>
-        <a href="index.php">Home</a> | 
+        <a href="index.php">Home</a> |
+        <a href="clubs.php">Clubs</a> |
         <?php if (isset($_SESSION['username'])): ?>
             <a href="logout.php">Logout (<?= htmlspecialchars($_SESSION['username']) ?>)</a>
         <?php else: ?>
-            <a href="login.php">Login</a> | 
+            <a href="login.php">Login</a> |
             <a href="register.php">Register</a>
         <?php endif; ?>
     </nav>
