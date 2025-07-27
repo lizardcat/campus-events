b41d9e4e621112235fd9d772ac5c82af788cec98
lizardcat <alex.raza.ke@gmail.com>
2025-07-27 14:07:09 +0300

    Adding placeholder images

--- includes/footer.php
+++ includes/footer.php
@@ -2,7 +2,7 @@
 
 <footer class="bg-dark text-white text-center py-3 mt-5">
     <div class="container">
-        <p class="mb-0">&copy; <?= date('Y') ?> USIU Campus Events System</p>
+        <p class="mb-0">&copy; <?= date('Y') ?>. USIU Campus Events System</p>
     </div>
 </footer>
 
